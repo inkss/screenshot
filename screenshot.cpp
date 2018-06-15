ef95d6e4a39171f48f5766d6cd417d5e795e8921
buting <szhiy1996@gmail.com>
2018-06-15 19:29:09 +0800

    补充注释

--- screenshot.cpp
+++ screenshot.cpp
@@ -5,44 +5,48 @@
 Screenshot::Screenshot()
     :  screenshotLabel(new QLabel(this))
 {
-    screenshotLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    screenshotLabel->setAlignment(Qt::AlignCenter);
+    screenshotLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding); //设置控件在布局（layout）里面的大小变化的属性 水平和垂直方向拉伸
+    screenshotLabel->setAlignment(Qt::AlignCenter); //设置对齐：居中对齐
 
-    const QRect screenGeometry = QApplication::desktop()->screenGeometry(this);
-    screenshotLabel->setMinimumSize(screenGeometry.width() / 8, screenGeometry.height() / 8);
+    const QRect screenGeometry = QApplication::desktop()->screenGeometry(this); //获取屏幕尺寸
+    screenshotLabel->setMinimumSize(screenGeometry.width() / 8, screenGeometry.height() / 8); //设置窗口最小尺寸
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(screenshotLabel);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this); //布局管理器 垂直排列布局
+    mainLayout->addWidget(screenshotLabel); //添加控件：QLabel 用于显示屏幕截图预览
 
-    QGroupBox *optionsGroupBox = new QGroupBox(tr("Options"), this);
-    delaySpinBox = new QSpinBox(optionsGroupBox);
-    delaySpinBox->setSuffix(tr(" s"));
-    delaySpinBox->setMaximum(60);
+    QGroupBox *optionsGroupBox = new QGroupBox(tr("Options"), this); //组合框
+
+    delaySpinBox = new QSpinBox(optionsGroupBox); //实例化一个QSpinBox 只能设置整数 浮点数是 QDoubleSpinBox
+    delaySpinBox->setSuffix(tr(" s")); //设置后缀
+    delaySpinBox->setMaximum(60); //设置最大值
 
     connect(delaySpinBox, QOverload<int>::of(&QSpinBox::valueChanged),
-            this, &Screenshot::updateCheckBox);
+            this, &Screenshot::updateCheckBox); //连接到 updateCheckBox 插槽
 
-    hideThisWindowCheckBox = new QCheckBox(tr("Hide This Window"), optionsGroupBox);
+    hideThisWindowCheckBox = new QCheckBox(tr("Hide This Window"), optionsGroupBox); //实例化一个 QCheakBox
 
-    QGridLayout *optionsGroupBoxLayout = new QGridLayout(optionsGroupBox);
-    optionsGroupBoxLayout->addWidget(new QLabel(tr("Screenshot Delay:"), this), 0, 0);
-    optionsGroupBoxLayout->addWidget(delaySpinBox, 0, 1);
-    optionsGroupBoxLayout->addWidget(hideThisWindowCheckBox, 1, 0, 1, 2);
+    QGridLayout *optionsGroupBoxLayout = new QGridLayout(optionsGroupBox); //表格排列布局
+    optionsGroupBoxLayout->addWidget(new QLabel(tr("Screenshot Delay:"), this), 0, 0); //添加一个 QLabel
+    optionsGroupBoxLayout->addWidget(delaySpinBox, 0, 1); //添加隐藏本窗口空间
+    optionsGroupBoxLayout->addWidget(hideThisWindowCheckBox, 1, 0, 1, 2); //添加复选框控件
 
-    mainLayout->addWidget(optionsGroupBox);
+    mainLayout->addWidget(optionsGroupBox); //将组合框给主样式
 
     QHBoxLayout *buttonsLayout = new QHBoxLayout;
     newScreenshotButton = new QPushButton(tr("New Screenshot"), this);
     connect(newScreenshotButton, &QPushButton::clicked, this, &Screenshot::newScreenshot);
     buttonsLayout->addWidget(newScreenshotButton);
+
     QPushButton *saveScreenshotButton = new QPushButton(tr("Save Screenshot"), this);
     connect(saveScreenshotButton, &QPushButton::clicked, this, &Screenshot::saveScreenshot);
     buttonsLayout->addWidget(saveScreenshotButton);
+
     QPushButton *quitScreenshotButton = new QPushButton(tr("Quit"), this);
     quitScreenshotButton->setShortcut(Qt::CTRL + Qt::Key_Q);
     connect(quitScreenshotButton, &QPushButton::clicked, this, &QWidget::close);
     buttonsLayout->addWidget(quitScreenshotButton);
     buttonsLayout->addStretch();
+
     mainLayout->addLayout(buttonsLayout);
 
     shootScreen();
